486b5c2234dabea54d033ec0d81ebf1624f5c652
Sergio López <sergiolf96@gmail.com>
2020-02-01 18:42:34 +0100

    Detail View added

---
+++ iDine/UI/ItemDetail.swift
@@ -0,0 +1,42 @@
+//
+//   ItemDetail.swift
+//  iDine
+//
+//  Created by Sergio López on 01/02/2020.
+//  Copyright © 2020 Sergio López. All rights reserved.
+//
+
+import SwiftUI
+
+struct ItemDetail: View {
+    
+    var item: MenuItem
+    
+    var body: some View {
+        VStack {
+            ZStack(alignment: .bottomTrailing) {
+                Image(item.mainImage)
+                Text("Photo by: \(item.photoCredit)")
+                    .padding(4)
+                    .background(Color.black)
+                    .font(.caption)
+                    .foregroundColor(.white)
+                    .offset(x: -5, y: -5)
+                    
+            }
+            
+            Text(item.description)
+                .padding()
+            Spacer()
+        }.navigationBarTitle(Text(item.name), displayMode: .inline)
+    }
+}
+
+struct ItemDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ItemDetail(item: MenuItem.example)
+        }
+        
+    }
+}
