64f5ead3105f4439ecb3f83ace2b0818b45296ef
Sergio López <sergiolf96@gmail.com>
2020-02-01 12:26:38 +0100

    Changes in files

--- iDine/UI/MenuItem.swift
+++ iDine/UI/MenuItem.swift
@@ -1,18 +1,12 @@
 //
-//  Menu.swift
+//  MenuItem.swift
 //  iDine
 //
-//  Created by Paul Hudson on 27/06/2019.
-//  Copyright © 2019 Hacking with Swift. All rights reserved.
+//  Created by Sergio López on 01/02/2020.
+//  Copyright © 2020 Sergio López. All rights reserved.
 //
 
 import SwiftUI
-
-struct MenuSection: Codable, Identifiable {
-    var id: UUID
-    var name: String
-    var items: [MenuItem]
-}
 
 struct MenuItem: Codable, Equatable, Identifiable {
     var id: UUID
@@ -34,3 +28,4 @@ struct MenuItem: Codable, Equatable, Identifiable {
     static let example = MenuItem(id: UUID(), name: "Maple French Toast", photoCredit: "Joseph Gonzalez", price: 6, restrictions: ["G", "V"], description: "Sweet, fluffy, and served piping hot, our French toast is flown in fresh every day from Maple City, Canada, which is where all maple syrup in the world comes from. And if you believe that, we have some land to sell you…")
     #endif
 }
+
---
+++ iDine/UI/MenuSection.swift
@@ -0,0 +1,15 @@
+//
+//  Menu.swift
+//  iDine
+//
+//  Created by Paul Hudson on 27/06/2019.
+//  Copyright © 2019 Hacking with Swift. All rights reserved.
+//
+
+import SwiftUI
+
+struct MenuSection: Codable, Identifiable {
+    var id: UUID
+    var name: String
+    var items: [MenuItem]
+}
